f443076bc5a349a7565452110b1c409765fa5ca5
stiva98 <samustiva9810@gmail.com>
2023-08-22 13:20:00 +0200

    ADD php esercizio terminato

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Badwords</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Badwords</title>
+    </head>
 <body>
     <main>
         <h1>
             PHP Badwords
         </h1>
         <form action="results.php" method="GET">
-            <input type="text" name="textarea">
-            <input type="text" name="parolaOscurata">
+            <div>
+                <input type="text" name="parolaOscurata">
+            </div>
+            <div>
+                <textarea type="text" name="textarea"></textarea>
+            </div>
             <button type="submit">
                 INVIA
             </button>
--- results.php
+++ results.php
@@ -1,4 +1,33 @@
 <?php 
-    echo $_GET['textarea'];
-    echo $_GET['parolaOscurata'];
+    $textarea=$_GET['textarea'];
+    $parolaOscurata=$_GET['parolaOscurata'];
+    $parolaOscurataRisultato=str_ireplace($parolaOscurata,'***',$textarea);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Results</title>
+    </head>
+<body>
+    <main>
+        <h3>
+            <?php echo $textarea; ?>
+        </h3>
+        <h3>
+            <?php echo $parolaOscurata; ?>
+        </h3>
+        <h3>
+            La lunghezza del paragrafo è: <?php echo strlen($textarea); ?>
+        </h3>
+        <h3>
+            <?php echo $parolaOscurataRisultato; ?>
+        </h3>
+        <h3>
+            La lunghezza del paragrafo è: <?php echo strlen($parolaOscurataRisultato); ?>
+        </h3>
+    </main>
+    
+</body>
+</html>
